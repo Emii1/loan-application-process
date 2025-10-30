71fe98de1b0fec632a81bb66fa4b55f6700cac78
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2025-10-30 13:58:25 +0100

    revision

--- pm4py/algo/conformance/alignments/petri_net/algorithm.py
+++ pm4py/algo/conformance/alignments/petri_net/algorithm.py
@@ -260,7 +260,7 @@ def apply_log(
         parameters[Parameters.BEST_WORST_COST_INTERNAL] = best_worst_cost
 
     is_dijkstra = str(variant) in {"Variants.VERSION_DIJKSTRA_LESS_MEMORY", "Variants.VERSION_DIJKSTRA_NO_HEURISTICS"}
-    thm = thread_utils.Pm4pyThreadManager(is_threaded=is_dijkstra)
+    thm = thread_utils.Pm4pyThreadManager(is_threaded=(is_dijkstra and constants.DEFAULT_IS_THREADING_MANAGEMENT_ENABLED))
 
     all_alignments = [None] * len(one_tr_per_var)
 
